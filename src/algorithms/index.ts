3ca9bb25f62036dcb4109fcdcf913f24d1f538f7
Serge <sajghaa@gmail.com>
2026-09-22 20:54:30 +0200

    update index.ts

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -1,10 +1,12 @@
 import type { SortingAlgorithm } from './types';
 import { bubbleSort } from './sorting/bubbleSort';
 import { mergeSort } from './sorting/mergeSort';
+import { quickSort } from './sorting/quickSort';
 
 export const sortingAlgorithms: SortingAlgorithm[] = [
   bubbleSort,
   mergeSort,
+  quickSort,
 ];
 
 export type { AlgorithmStep, SortingAlgorithm, AlgorithmInfo } from './types';
